fad277cbbc684e75e9a3ea3861ed896d513f8422
Alexander <fenaethylaamiinloever@gmail.com>
2024-11-24 16:24:36 +0300

    Начинаем программировать

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import MainPage from '../../pages/main-page/main-page';
+
+// ^======================== App ========================^ //
+
+export default function App() {
+  return (
+    <div className='app'>
+      <MainPage />
+    </div>
+  );
+}
