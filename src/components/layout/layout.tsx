555832a8ac14d73134c7c43f8a20483b355ccf06
Alexander <fenaethylaamiinloever@gmail.com>
2024-11-27 19:38:40 +0300

    layout fixed

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,20 @@
-// ^======================== Layout ========================^ //
-
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Logo from '../logo/logo';
 import HeaderNav from '../header-nav/header-nav';
+import { AppRoute } from '../../const';
 
+// ^======================== Layout ========================^ //
 
 export default function Layout(): JSX.Element {
+
+  const path = useLocation().pathname as AppRoute;
+  const isGrayPage = path === AppRoute.Main || path === AppRoute.Login;
+  const pageClassName = isGrayPage ? 'page page--gray' : 'page';
+
   return (
-    <>
+    <div
+      className={pageClassName}
+    >
       <header className='header'>
         <div className='container'>
           <div className='header__wrapper'>
@@ -27,6 +34,6 @@ export default function Layout(): JSX.Element {
       <footer>
 
       </footer>
-    </>
+    </div>
   );
 }
