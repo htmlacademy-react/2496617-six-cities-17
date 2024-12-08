f23a8e0b76b62703e8e99f09be75f9bffb711a11
Alexander <fenaethylaamiinloever@gmail.com>
2024-12-08 21:21:40 +0300

    fix: + condition for singular

--- src/components/offer-header/offer-header.tsx
+++ src/components/offer-header/offer-header.tsx
@@ -43,10 +43,10 @@ export default function OfferHeader(offerHeaderProps: OfferHeaderProps): JSX.Ele
       <ul className='offer__features'>
         <li className='offer__feature offer__feature--entire'>{capitalize(type)}</li>
         <li className='offer__feature offer__feature--bedrooms'>
-          {bedrooms} Bedrooms
+          {`${bedrooms} Bedroom${bedrooms > 1 ? 's' : ''}`}
         </li>
         <li className='offer__feature offer__feature--adults'>
-          Max {maxAdults} adults
+          {`Max ${maxAdults} adult${maxAdults > 1 ? 's' : ''}`}
         </li>
       </ul>
 
