9c5054389802806853a6f32fe487c09e88bbe540
Alexander <fenaethylaamiinloever@gmail.com>
2024-12-08 21:42:47 +0300

    fix: form submit condition

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -26,6 +26,11 @@ export default function ReviewForm(): JSX.Element {
     setReviewFormState(reviewFormInitialState);
   };
 
+  const submitCondition: boolean =
+    Boolean(rating) && !text
+    || Boolean(rating) && (text.length >= 50 && text.length < 300)
+    || (text.length >= 50 && text.length < 300);
+
   return (
     <form
       className='reviews__form form'
@@ -69,7 +74,7 @@ export default function ReviewForm(): JSX.Element {
         <button
           className='reviews__submit form__submit button'
           type='submit'
-          disabled={!(rating || text.length >= 50)}
+          disabled={submitCondition}
         >
           Submit
         </button>
