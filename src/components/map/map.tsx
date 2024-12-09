6b74b72ee8682ad6617f4cc8af52e86743127126
alexx-kis <kislov@advline.ru>
2024-12-09 09:39:40 +0300

    markers layer added to map

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from '../../const';
 import { useEffect, useRef } from 'react';
-import leaflet from 'leaflet';
+import leaflet, { layerGroup, Marker } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { PlaceCardType, LocationType } from '../../types';
 import { useMap } from '../../hooks/use-map';
@@ -36,20 +36,20 @@ export default function Map({ defaultLocation, offers, selectedPoint }: MapProps
   useEffect(() => {
 
     if (map && offers) {
+      const markerLayer = layerGroup().addTo(map);
+
       offers.forEach((offer) => {
-        leaflet
-          .marker(
-            {
-              lat: offer.location.latitude,
-              lng: offer.location.longitude,
-            },
-            {
-              icon: offer.id === selectedPoint?.id
-                ? currentCustomIcon
-                : defaultCustomIcon,
-            }
+        const marker = new Marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude,
+        });
+
+        marker
+          .setIcon(offer.id === selectedPoint?.id
+            ? currentCustomIcon
+            : defaultCustomIcon,
           )
-          .addTo(map);
+          .addTo(markerLayer);
       });
     }
   }, [map, offers, selectedPoint, defaultCustomIcon, currentCustomIcon]);
