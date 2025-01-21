237960dfcc933fc75656130b53af390b267d2fed
alexx-kis <kislov@advline.ru>
2025-01-21 14:43:56 +0300

    fix: main page test corrected

--- src/pages/main-page/main-page.test.tsx
+++ src/pages/main-page/main-page.test.tsx
@@ -5,7 +5,7 @@ import { makeFakeStore } from '../../utils/mocks';
 import MainPage from './main-page';
 
 describe('Page: MainPage', () => {
-  it('Should render correctly', () => {
+  it('Should render correctly if there are offers', () => {
     const expectedHeadingText = 'Cities';
     const mainPageTestId = 'main-page-element';
 
@@ -15,6 +15,18 @@ describe('Page: MainPage', () => {
     render(withHistoryComponent);
 
     expect(screen.getByText(expectedHeadingText));
+    expect(screen.getByTestId(mainPageTestId));
+  });
+
+  it('Should render correctly if there are no offers', () => {
+    const expectedText = 'No places to stay available';
+    const mainPageTestId = 'main-page-element';
+    const { withStoreComponent } = withStore(<MainPage />, makeFakeStore());
+    const withHistoryComponent = withHistory(withStoreComponent);
+
+    render(withHistoryComponent);
+
+    expect(screen.getByText(expectedText));
     expect(screen.getByTestId(mainPageTestId));
   });
 });
