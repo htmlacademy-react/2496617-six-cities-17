7a9dc6fcb5612c078258cb4974f87aae35694b32
alexx-kis <kislov@advline.ru>
2025-01-22 18:07:35 +0300

    fixe: utils functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -22,11 +22,11 @@ export const sortOffers = (offers: PlaceCardType[], sortingType: SortingOption):
     case SortingOption.Popular:
       return offers;
     case SortingOption.ByLowPrice:
-      return [...offers.sort(compareOffersByLowPrice)];
+      return [...offers].sort(compareOffersByLowPrice);
     case SortingOption.ByHighPrice:
-      return [...offers.sort(compareOffersByHighPrice)];
+      return [...offers].sort(compareOffersByHighPrice);
     case SortingOption.ByRating:
-      return [...offers.sort(compareOffersByRating)];
+      return [...offers].sort(compareOffersByRating);
 
     default:
       return offers;
@@ -34,10 +34,10 @@ export const sortOffers = (offers: PlaceCardType[], sortingType: SortingOption):
 };
 
 export const sortReviews = (reviews: ReviewType[]): ReviewType[] =>
-  [...reviews.sort(
+  [...reviews].sort(
     (a: ReviewType, b: ReviewType) =>
       new Date(b.date).getTime() - new Date(a.date).getTime()
-  )];
+  );
 
 export const updateFavoriteStatus = (offers: PlaceCardType[], payload: PlaceCardType, isFavorite: boolean) => offers.map((offer) => offer.id === payload.id ? { ...offer, isFavorite } : offer);
 
