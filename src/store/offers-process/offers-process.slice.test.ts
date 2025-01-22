a3ec9372d29ed3f7a3c7048ca2290ed29e9a68ba
alexx-kis <kislov@advline.ru>
2025-01-22 17:58:16 +0300

    fix: console.log removed

--- src/store/offers-process/offers-process.slice.test.ts
+++ src/store/offers-process/offers-process.slice.test.ts
@@ -74,7 +74,6 @@ describe('OffersProcess Slice', () => {
       sortingType: SortingOption.Popular,
       status: DataStatus.Unknown,
     };
-    console.log(initialState.sorted)
 
     const expectedState = {
       cityName: DEFAULT_CITY_NAME,
