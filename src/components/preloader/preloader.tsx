c28455ecb4f8f9f62504089cb4c412840e4e6331
alexx-kis <kislov@advline.ru>
2025-01-24 14:55:45 +0300

    ref: additional inline styles moved to separate files

--- src/components/preloader/preloader.tsx
+++ src/components/preloader/preloader.tsx
@@ -1,25 +1,11 @@
+import './preloader.css';
+
 // ^======================== Preloader ========================^ //
 
 export default function Preloader(): JSX.Element {
   return (
-    <div
-      className='preloader__wrapper'
-      style={{
-        position: 'fixed',
-        inset: 0,
-        zIndex: '10',
-        display: 'grid',
-        placeItems: 'center',
-        backgroundColor: '#ffffff80',
-        backdropFilter: 'blur(4px)'
-      }}
-    >
-      <div
-        className='preloader'
-        style={{
-          fontSize: 100,
-        }}
-      >
+    <div className='preloader__wrapper'>
+      <div className='preloader' >
         Loading...
       </div>
     </div>
